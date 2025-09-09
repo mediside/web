376d63932cc153360f1be3933752eede81355f11
Maksim <lopatinma@mts.ru>
2025-09-10 01:45:50 +0300

    chore(package): support alias import

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@public': resolve('public'),
+      '@public/*': resolve('public/*'),
+      '@shared': resolve('src/shared'),
+      '@entities': resolve('src/entities'),
+      '@features': resolve('src/features'),
+      '@widgets': resolve('src/widgets'),
+      '@pages': resolve('src/pages'),
+      '@app': resolve('src/app'),
+    },
+  },
 })
