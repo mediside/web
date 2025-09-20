f85db05d372521799c81e697f174719ae5063990
Maksim <lopatinma@mts.ru>
2025-09-20 18:44:42 +0300

    chore(widgets): change useTranslation usage

--- src/widgets/topbar/Topbar.tsx
+++ src/widgets/topbar/Topbar.tsx
@@ -1,14 +1,12 @@
 import { Box, Center, Heading, useTranslation } from '@shared'
 
 export const Topbar: FC = () => {
-  const t = {
-    common: useTranslation('common'),
-  }
+  const t = useTranslation('common')
 
   return (
     <Center>
       <Box bg="gray.contrast" shadow="md" w="full" mb={6} p={2} h={12}>
-        <Heading>{t.common('product')}</Heading>
+        <Heading>{t('product')}</Heading>
       </Box>
     </Center>
   )
