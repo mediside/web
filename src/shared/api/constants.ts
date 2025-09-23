e9f99c3262c6fa180ced34ff7b2ad1cbbe910961
Maksim <lopatinma@mts.ru>
2025-09-23 21:40:27 +0300

    chore(package): set DEV and PROD API_URL

--- src/shared/api/constants.ts
+++ src/shared/api/constants.ts
@@ -1,4 +1,6 @@
-export const API_URL = 'http://localhost:9042/api/v1/'
+const IS_DEV = import.meta.env.DEV
+
+export const API_URL = import.meta.env[IS_DEV ? 'VITE_DEV_API_URL' : 'VITE_PROD_API_URL']
 
 export enum Method {
   GET = 'GET',
